89a8aff33235e918255ddfead00149291b60be2d
hemantk4413 <hemantkumbhar328@gmail.com>
2025-10-12 15:42:18 +0530

    commit one and push

--- simple-python-proj/scripts/start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull hemant4413/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 hemant4413/simple-python-flask-app
-
